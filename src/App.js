2fe6c035277a1e282aaa65e667f3e46b12a234ec
adriano <adriano.faria@outlook.com.br>
2018-05-23 19:02:44 -0300

    Teste para verificar se consigo resposta do api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import './App.css';
 import axios from "axios";
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get(`http://${window.location.hostname}/api/users/list`)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
